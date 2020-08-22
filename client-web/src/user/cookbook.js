67cc9bd3b6c417ca9d336d215482aff7b2d6a076
Janscode <55261974+Janscode@users.noreply.github.com>
2020-08-22 14:17:50 -0600

    more tweaks

--- client-web/src/user/cookbook.js
+++ client-web/src/user/cookbook.js
@@ -5,7 +5,7 @@ import Recipe from './recipe'
 export default class Cookbook extends Component{
     render(){
         const recipeList = this.props.cookbookJson.recipes.map((recipe) => 
-        <Recipe recipe={recipe} key={recipe.name}/>
+        <Recipe recipe={recipe} username={this.props.username} key={recipe.name}/>
     );
         
         return(
