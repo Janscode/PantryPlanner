cd1ac70c26bc644be61ad0368622a1e46fa6218e
Janscode <55261974+Janscode@users.noreply.github.com>
2020-08-22 17:39:10 -0600

    test using componentDidMount

--- client-web/src/user/pantry.js
+++ client-web/src/user/pantry.js
@@ -1,6 +1,20 @@
-import React, {Componen, Component} from 'react';
+import React, {Component} from 'react';
+import Axios from 'axios';
 
 export default class Pantry extends Component{
+    componentDidMount() {
+        formBody = new FormData();
+
+        Axios(
+            this.props.username
+        )
+        .then((response) => {
+            console.log(response)
+        });
+        .catch((error) =>{
+            console.log(error)
+        });
+    }
     render(){
         return(
             <h1>this is a Pantry</h1>
