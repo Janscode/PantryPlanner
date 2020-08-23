cf13da01d1b111940fc689f395a94f57750ccedd
Janscode <55261974+Janscode@users.noreply.github.com>
2020-08-22 22:18:28 -0600

    Rename pantry

--- client-web/src/user/shoppinglist.js
+++ client-web/src/user/shoppinglist.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
+import Order from './order'
 import Axios from 'axios';
 
-export default class Pantry extends Component{
+export default class Shoppinglist extends Component{
     state = {}
     componentDidMount() {
         var formBody = new FormData();
@@ -13,13 +14,19 @@ export default class Pantry extends Component{
             headers: {'Content-Type': 'multipart/form-data' }
         })
         .then((response) => {
-            console.log(response)
+            this.setState({
+                ordersPending: response.ordersPending,
+                ordersDelivered: response.ordersDelivered
+            });
         })
         .catch((error) =>{
             console.log(error)
         });
     }
     render(){
+        const ordersPendingList = this.state.ordersPending.map((order) =>
+            <li><Order order={order}/></li>
+        )
         return(
             <h1>this is a Pantry </h1>
 
