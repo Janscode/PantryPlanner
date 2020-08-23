7a5d25d5b3d389248e83b7a31441cf36e6440d68
Janscode <55261974+Janscode@users.noreply.github.com>
2020-08-23 13:13:29 -0600

    missed coma

--- server/api/urls.py
+++ server/api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('saveRecipe', views.saveRecipe, name='saveRecipe'),
     path('getRecipeList', views.getRecipeList, name='get recipe list'),
-    path('getOrderList', views.getOrderList, name='get order list')
+    path('getOrderList', views.getOrderList, name='get order list'),
     path('orderRecipe', views.orderRecipe, name='order recipe')
 ]
