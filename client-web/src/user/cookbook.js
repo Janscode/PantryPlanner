00e44ba7727af013df64cc8544ba7295a64272a9
Janscode <55261974+Janscode@users.noreply.github.com>
2020-08-24 01:52:17 -0600

    refactor api request

--- client-web/src/user/cookbook.js
+++ client-web/src/user/cookbook.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
-import Recipe from './recipe'
+import Recipe from './recipe';
+import Axios from 'axios';
 
 
 export default class Cookbook extends Component{
+    state ={
+        recipes: []
+    }
+    componentDidMount(){
+        var formBody = new FormData();
+        formBody.append("username", this.props.username);
+        Axios({
+            method: 'POST',
+            url: '/api/getRecipeList',
+            data: formBody,
+            headers: {'Content-Type': 'multipart/form-data' }
+          })
+          .then(
+              (response) => {this.setState({
+                  recipes: response.data.recipes
+              });
+            }
+          )
+          .catch(function (response){
+              console.log(response);
+          });
+    }
     render(){
-        const recipeList = this.props.cookbookJson.recipes.map((recipe) => 
+        const recipeList = this.state.recipes.map((recipe) => 
         <Recipe 
         recipe={recipe} 
         username={this.props.username} 
